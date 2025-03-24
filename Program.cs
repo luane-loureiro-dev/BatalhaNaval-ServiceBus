4a6e376cddc294f2017eadc74d3d1b88bc3ad9dc
luane-loureiro-dev <luane.loureiro@esx.com.br>
2025-03-24 16:04:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿static void Demo(int x, int y)
+{
+    x = 8
+        y=3
+    if (x < y)
+    {
+        Console.WriteLine(x + y);
+    }
+    else
+    {
+        Console.WriteLine(x % y);
+    }
+}
